1e3d4de4f2d27cee9b0f15cbae4ea0e9ab30d23f
Snaddyvitch-Dispenser <28988626+Snaddyvitch-Dispenser@users.noreply.github.com>
2020-05-03 22:05:25 +0100

    Update GitIgnore

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+var app = express.createServer();
+
+app.get("/", function (req, res) {
+
+});
+
+app.listen(80);
+console.log('Express server started on port %s', app.address().port);
